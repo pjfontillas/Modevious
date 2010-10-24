dc671a0bf2756036d8350eb97a0eb69922a792b7
pjfontillas <pjfontillas@gmail.com>
2010-10-24 15:27:00 -0500

    Made contact form dynamic like the download section.
    Works just like the download section, albeit more AJAX-heavy. Also degrades gracefully if JS is disabled. jquery.blockUI.js added for this, and I am considering adding it as a core component.

--- Website/dev/contact/ed/index.php
+++ Website/dev/contact/ed/index.php
@@ -1,15 +1,24 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/include/links.htm'); ?>
-  <title>Contact me</title>
-</head>
-<body>
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/include/navigation.htm'); ?>
-  <div id="core" class="ui-corner-all">
-    <div class="full">
-      <p class="padded">
 <?php
 $from = $_POST['name'];
 $fromEmail = $_POST['email'];
 $fromMessage = $_POST['message'];
+$ajax = isset($_POST['ajax']);
+if ($ajax != 1) {
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/links.htm'); 
+	echo '
+			<title>Contact me</title>
+		</head>
+		<body>
+	';
+}
+if ($ajax != 1) {
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/navigation.htm');
+	echo '
+		<div id="core" class="ui-corner-all">
+			<div class="full">
+				<p class="padded">
+	';
+}
 $ip = $_SERVER['REMOTE_ADDR'];
 $time = time();
 $maintenance = "me@pjfontillas.com";
@@ -18,15 +27,29 @@ $subject = "Message from $from";
 $econtent = "$fromMessage\n\nSent on " . date("D F d Y", $time) . " from $ip";
 $headers = "From: $from <$fromEmail>";
 if(mail($to,$subject,$econtent,$headers)) {
-echo "Your message was successfully sent! I'll get back to you as soon as possible!";
+	if ($ajax) {
+		echo "{\"status\":\"success\"}";
+	} else {
+		echo "Your message was successfully sent! I'll get back to you as soon as possible!";
+	}
 } else {
-echo "There was a problem sending your message. Please contact $maintenance";
+	if ($ajax) {
+		echo "{\"status\":\"failure\"}";
+	} else {
+		echo "There was a problem sending your message. Please contact $maintenance";
+	}
 }    
+if ($ajax != 1) {
+	echo '
+			</p>
+		</div>
+		<div class="clear-float"></div>      
+	</div><!-- End #core -->
+	';
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/footer.htm');
+	echo '
+			</body>
+		</html>
+		';
+}
 ?>
-      </p>
-    </div>
-    <div class="clear-float"></div>      
-  </div><!-- End #core -->
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/include/footer.htm'); ?>
-</body>
-</html>
--- Website/dev/js/main.js
+++ Website/dev/js/main.js
@@ -1,10 +1,11 @@
+$c.include("/js/jquery.blockUI.js");
+$c.trace("Loaded jquery.blockUI.js");
 $c.onLoad(function () {
-	$c.trace("Starting behavior configuration for #menu-download");
 	$("menu-download").observe("click", function (event) {
 		$c.trace("Download link pressed, stopping default link behavior");
 		event.stop();
 		var downloadContainer = $j("#download-container");
-		if ($j("#download-container").is(":hidden")) {
+		if (downloadContainer.is(":hidden")) {
 			$c.trace("#download-container was hidden, now showing");
 			downloadContainer.show().css({
 				left: (document.viewport.getWidth() - downloadContainer.outerWidth(true)) / 2 
@@ -21,18 +22,100 @@ $c.onLoad(function () {
 		].join(''));
 		downloadContainer.animate({
 			top: document.viewport.getScrollOffsets().top + 50
-		}).expose({
-			closeOnClick: false,
-			closeOnEsc: false,
-			color: "#000"
 		});
 	});
-	$("close-download").observe("click", function () {
+	$("hide-download").observe("click", function () {
 			$c.trace("Hiding #download-container");
 			$j("#download-container").animate({
 				top: -1000
 			});
-			$j.mask.close();
 	});
 	$c.trace("Finished configuring behavior for #menu-download");
+	$("menu-contact").observe("click", function (event) {
+		event.stop();
+		var contactContainer = $j("#contact-container");
+		if (contactContainer.is(":hidden")) {
+			$c.trace("#contact-container was hidden, now showing");
+			contactContainer.show().css({
+				left: (document.viewport.getWidth() - contactContainer.outerWidth(true)) / 2 
+			}).draggable();
+		}
+		$c.trace("Moving #contact-container");
+		$c.trace([
+			"document.viewport.getWidth(): ",
+			document.viewport.getWidth()
+		].join(''));
+		$c.trace([
+			"contactContainer.outerWidth(true): ",
+			contactContainer.outerWidth(true)
+		].join(''));
+		contactContainer.animate({
+			top: document.viewport.getScrollOffsets().top + 50
+		});	
+	});
+	$("submit-contact-form").observe("click", function (event) {
+		event.stop();
+		$c.trace("Contact form was submitted");
+		$j("#contact-container").block({
+			message: "<p>Sending message...</p>"
+		});
+		var ajaxRequest = new Ajax.Request("/contact/ed/", {
+			parameters: {
+				name: $F("contact-name"),
+				message: $F("contact-message"),
+				email: $F("contact-email"),
+				ajax: "true"
+			},
+			onSuccess: function (transport) {
+				var fn;
+				var data = transport.responseText.evalJSON();
+				$c.trace("Ajax Request response: " + data);
+				$c.trace("Ajax Request response.status: " + data.status);
+				if (data.status == "success") {
+					$j("#contact-container").block({
+						message: "<p>Message sent!</p>"
+					});
+					fn = function () {
+						$j("#contact-container").unblock().animate({
+							top: -1000
+						});
+						$("contact-name").value = '';
+						$("contact-message").value = '';
+						$("contact-email").value = '';
+					};
+				} else {
+					$j("#contact-container").block({
+						message: "<p>Something went wrong, please try again or wait a while before resubmitting through this form!</p>"
+					});
+					fn = function () {
+						$j("#contact-container").unblock();
+					};
+				}
+				setTimeout(fn, 3000);
+			},
+			onFailure: function (transport) {
+				$c.trace("There was a problem with the Ajax Request, here is the response status: " + transport.status);
+			}
+		});
+	});
+	$("cancel-contact-form").observe("click", function (event) {
+		event.stop();
+		$c.trace("Contact form was cancelled");
+		$j("#contact-container").animate({
+			top: -1000
+		});
+	});
+	$c.trace("Finished configuring behavior for #contact-download");
+	$j("#download-container").click(function () {
+		if ($j("#contact-container").is(":visible")) {
+			$j("#download-container").css("zIndex", 1002);
+			$j("#contact-container").css("zIndex", 1001);
+		}
+	});
+	$j("#contact-container").click(function () {
+		if ($j("#download-container").is(":visible")) {
+			$j("#contact-container").css("zIndex", 1002);
+			$j("#download-container").css("zIndex", 1001);
+		}
+	});
 });
