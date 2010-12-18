20de0bb23bf9e8518160f3afe52bd1f7dd0202e1
pjfontillas <pjfontillas@gmail.com>
2010-12-17 19:14:05 -0600

    Removed $c.onLoad.
    Pivotal Tracker story:
    https://www.pivotaltracker.com/story/show/7390435

--- Development/js/src/core.js
+++ Development/js/src/core.js
@@ -174,17 +174,7 @@ var $c = (function () {
 		*		function after the page is done loading.
 		*/
 		addLoadEvent: function (func) {
-			var oldonload = window.onload;
-			if (typeof window.onload !== "function") {
-				window.onload = func;
-			} else {
-				window.onload = function () {
-					if (oldonload) {
-						oldonload();
-					}
-					func();
-				};
-			}
+			$j("document").ready(func);
 		},
 		onLoad: function (func) {
 			$c.addLoadEvent(func);
