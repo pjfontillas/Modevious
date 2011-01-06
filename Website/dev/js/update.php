2c15134fcb2d93cd31f616146c275f950369b2d5
pjfontillas <pjfontillas@gmail.com>
2011-01-05 23:47:40 -0600

    Added copy of update.php to dev section. Useful for when completely wiping the root directory of the production server and populating it with content from dev.

---
+++ Website/dev/js/update.php
@@ -0,0 +1,8 @@
+<?php echo($_GET['callback']); ?>({
+  "version": "122",
+  "versionString": "v1.2.2",
+  "description": "Version 1.2.2 adds Google Code Prettify and jQuery Context Menu support and enhances automatically created draggables by the use of the draggables class.",
+  "url": "http://modevious.com/cache/modevious.1.2.2.zip",
+  "changelog": "http://github.com/pjfontillas/modevious/commits/master",
+  "type": "Normal"
+});
