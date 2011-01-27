1907f1d2e2f02a5bf1dec48170b5ee52a71ebe1b
pjfontillas <pjfontillas@gmail.com>
2011-01-26 19:56:20 -0600

    Fixed the weird character problem for real this time. Need to keep an eye out for that.

--- compile.bat
+++ compile.bat
@@ -10,58 +10,58 @@ type nul > tmp\library.js
 type nul > tmp\library.css
 
 :: Prototype JS
-copy /b tmp\library.js + Development\js\src\prototype.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\prototype.js tmp\library.js
 
 :: jQuery
-copy /b tmp\library.js + Development\js\src\jquery%1.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\jquery%1.js tmp\library.js
 
 :: Core libary
 mkdir tmp\console
 copy /b Development\css\console\* tmp\console\
-copy /b tmp\library.css + Development\css\core%1.css tmp\library.css
-copy /b tmp\library.js + Development\js\src\core%1.js tmp\library.js
+copy /a tmp\library.css + Development\css\core%1.css tmp\library.css
+copy /a tmp\library.js + Development\js\src\core%1.js tmp\library.js
 
 :: jQuery User interface
 mkdir tmp\images
 copy /b Development\css\images\* tmp\images\
 copy /b Development\css\jquery-ui%1.css tmp\jquery-ui.css
-copy /b tmp\library.js + Development\js\src\jquery-ui.min.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\jquery-ui.min.js tmp\library.js
 
 :: jQuery Tools
-copy /b tmp\library.js + Development\js\src\jquery.tools.min.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\jquery.tools.min.js tmp\library.js
 
 :: autoMouseOver jQuery plugin
-copy /b tmp\library.js + Development\js\src\jquery.autoMouseOver%1.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\jquery.autoMouseOver%1.js tmp\library.js
 
 :: Pines Notify jQuery plugin
-copy /b tmp\library.css + Development\css\jquery.pnotify.default%1.css tmp\library.css
-copy /b tmp\library.js + Development\js\src\jquery.pnotify%1.js tmp\library.js
+copy /a tmp\library.css + Development\css\jquery.pnotify.default%1.css tmp\library.css
+copy /a tmp\library.js + Development\js\src\jquery.pnotify%1.js tmp\library.js
 
 :: SoundManager 2 component
 mkdir tmp\swf
-copy /b tmp\library.js + Development\js\src\soundmanager2-nodebug-jsmin.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\soundmanager2-nodebug-jsmin.js tmp\library.js
 copy /b Development\swf\* tmp\swf\
 
 :: Encryption components
-copy /b tmp\library.js + Development\js\src\md5%1.js tmp\library.js
-copy /b tmp\library.js + Development\js\src\ripemd160%1.js tmp\library.js
-copy /b tmp\library.js + Development\js\src\sha1%1.js tmp\library.js
-copy /b tmp\library.js + Development\js\src\sha256%1.js tmp\library.js
-copy /b tmp\library.js + Development\js\src\sha512%1.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\md5%1.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\ripemd160%1.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\sha1%1.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\sha256%1.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\sha512%1.js tmp\library.js
 
 :: Right-click jQuery Context Menu
-copy /b tmp\library.js + Development\js\src\jquery.contextMenu%1.js tmp\library.js
-copy /b tmp\library.css + Development\css\jquery.contextMenu%1.css tmp\library.css
+copy /a tmp\library.js + Development\js\src\jquery.contextMenu%1.js tmp\library.js
+copy /a tmp\library.css + Development\css\jquery.contextMenu%1.css tmp\library.css
 
 :: blockUI
-copy /b tmp\library.js + Development\js\src\jquery.blockUI.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\jquery.blockUI.js tmp\library.js
 
 :: jQuery.Validate
-copy /b tmp\library.js + Development\js\src\jquery.validate%1.js tmp\library.js
+copy /a tmp\library.js + Development\js\src\jquery.validate%1.js tmp\library.js
 copy /b Development\css\jquery.validate\* tmp\jquery.validate\
 
 :: Startup script (init)
-copy /b tmp\library.js + Development\js\init%1.js tmp\library.js
+copy /a tmp\library.js + Development\js\init%1.js tmp\library.js
 
 :: Modevious Update System (MUpS)
 mkdir tmp\update
