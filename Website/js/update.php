5c85c52b8089888d3a8ea000d17258c63a7a6149
pjfontillas <pjfontillas@gmail.com>
2011-02-10 20:33:20 -0600

    Prep Modevious version 1.3.3 for release
    Pivotal Tracker story:
    https://www.pivotaltracker.com/story/show/9880013rt

--- Website/js/update.php
+++ Website/js/update.php
@@ -1,8 +1,8 @@
 <?php echo($_GET['callback']); ?>({
-  "version": "132",
-  "versionString": "v1.3.2",
-  "description": "Fixed location sniffer bug and problem with deployment build.",
-  "url": "http://modevious.com/cache/modevious.1.3.2.zip",
+  "version": "133",
+  "versionString": "v1.3.3",
+  "description": "Bug fixes and code reorganization. Updated Bash scripts for Unix based systems.",
+  "url": "http://modevious.com/cache/modevious.1.3.3.zip",
   "changelog": "http://github.com/pjfontillas/modevious/commits/master",
   "type": "Critical"
 });
